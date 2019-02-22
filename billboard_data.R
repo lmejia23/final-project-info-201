3aee7b9f990d385d5385b48be4a64c4f5429bcf3
Shea Frantz <sheafrantz@gmail.com>
2019-02-22 14:59:46 -0800

    added billboard data and preliminary R script

---
+++ billboard_data.R
@@ -0,0 +1,8 @@
+library(jsonlite)
+
+billboard_60 <- fromJSON("years/1960.json")
+billboard_70 <- fromJSON("years/1970.json")
+billboard_80 <- fromJSON("years/1980.json")
+billboard_90 <- fromJSON("years/1990.json")
+billboard_00 <- fromJSON("years/2000.json")
+billboard_10 <- fromJSON("years/2010.json")
