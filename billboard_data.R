21fbbf782143b11e915625143c863b201f90a1dd
KJTx1 <jasonyanzhizhen@gmail.com>
2019-02-24 14:20:59 -0800

    Update the combine function in the section 3

---
+++ billboard_data.R
@@ -0,0 +1,27 @@
+library(jsonlite)
+library(dplyr)
+
+# Examples of how to get data-frames from the individual .json files. 
+
+billboard_60 <- fromJSON("years/1960.json")
+billboard_70 <- fromJSON("years/1970.json")
+billboard_80 <- fromJSON("years/1980.json")
+billboard_90 <- fromJSON("years/1990.json")
+billboard_00 <- fromJSON("years/2000.json")
+billboard_10 <- fromJSON("years/2010.json")
+billboard_11 <- fromJSON("years/2011.json")
+billboard_12 <- fromJSON("years/2012.json")
+billboard_13 <- fromJSON("years/2013.json")
+billboard_14 <- fromJSON("years/2014.json")
+billboard_15 <- fromJSON("years/2015.json")
+
+# Function which gets a specific data frame for a given year
+
+get_year <- function(year) {
+  billboard <- fromJSON(paste0("years/", year, ".json"))
+  billboard <- billboard[1:50, ]
+  billboard <- select(billboard, title, artist, pos, num_words, tags)
+  billboard
+}
+
+
